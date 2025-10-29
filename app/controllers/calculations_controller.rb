dd8294cdfd13be81079e4b42b38fcb1d4dc5d70c
kouta <kouta.nbahand@gmail.com>
2025-10-29 20:53:05 +0900

    計算結果画面作成、計算結果の出力について修正

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -5,15 +5,25 @@ class CalculationsController < ApplicationController
     @calculation = Calculation.new
   end
 
-def preview
-  @calculation = Calculation.new(calculation_params)
-  @cf = @calculation.operating_profit.to_f + @calculation.depreciation.to_f
-  @debt_service_years = @cf > 0 ? (@calculation.borrowing.to_f / @cf).round(2) : nil
-  render :result
-end
+  def preview
+    @calculation = Calculation.new(calculation_params)
 
+    operating_profit = params[:calculation][:operating_profit].delete(',').to_f
+    depreciation     = params[:calculation][:depreciation].delete(',').to_f
+    borrowing        = params[:calculation][:borrowing].delete(',').to_f
 
-  # 実際に保存
+    @cf = @calculation.operating_profit.to_f + @calculation.depreciation.to_f
+    @debt_service_years = @cf > 0 ? (@calculation.borrowing.to_f / @cf).round(2) : nil
+
+    @calculation = Calculation.new(
+      operating_profit: operating_profit,
+      depreciation: depreciation,
+      borrowing: borrowing
+    )
+
+    render :result
+  end
+
   def create
     @calculation = current_user.calculations.new(calculation_params)
     if @calculation.save
@@ -27,5 +37,7 @@ end
 
   def calculation_params
     params.require(:calculation).permit(:operating_profit, :depreciation, :borrowing)
+          .transform_values { |v| v.delete(',').to_f }
   end
 end
+
