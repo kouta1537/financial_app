d57cbb3d3160493f8f0204031f9d2ec7dbd9752c
kouta <kouta.nbahand@gmail.com>
2025-11-14 01:56:50 +0900

    herokuへの接続のために修正

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_11_10_131311) do
-  create_table "calculations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "calculations", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "operating_profit"
     t.bigint "depreciation"
@@ -23,7 +26,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_11_10_131311) do
     t.index ["user_id"], name: "index_calculations_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
